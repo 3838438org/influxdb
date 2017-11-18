67bb28bddd781b1398bdf48a981f8a25fd0f2230
Jonathan A. Sternberg <jonathan@influxdb.com>
2017-11-18 17:30:48 -0600

    Add an auto choice that will choose the most appropriate logger

--- logger/logger.go
+++ logger/logger.go
@@ -6,26 +6,35 @@ import (
 	"time"
 
 	"github.com/jsternberg/zap-logfmt"
+	isatty "github.com/mattn/go-isatty"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 func New(w io.Writer) *zap.Logger {
-	return zap.New(zapcore.NewCore(
-		zaplogfmt.NewEncoder(newEncoderConfig()),
-		zapcore.Lock(zapcore.AddSync(w)),
-		zapcore.DebugLevel,
-	))
+	config := NewConfig()
+	l, _ := config.New(w)
+	return l
 }
 
 func (c *Config) New(defaultOutput io.Writer) (*zap.Logger, error) {
-	encoder, err := newEncoder(c.Format)
+	w := defaultOutput
+	format := c.Format
+	if c.Format == "" || c.Format == "auto" {
+		if isTerminal(w) {
+			format = "console"
+		} else {
+			format = "logfmt"
+		}
+	}
+
+	encoder, err := newEncoder(format)
 	if err != nil {
 		return nil, err
 	}
 	return zap.New(zapcore.NewCore(
 		encoder,
-		zapcore.AddSync(defaultOutput),
+		zapcore.AddSync(w),
 		c.Level,
 	)), nil
 }
@@ -37,7 +46,7 @@ func newEncoder(format string) (zapcore.Encoder, error) {
 		return zapcore.NewJSONEncoder(config), nil
 	case "console":
 		return zapcore.NewConsoleEncoder(config), nil
-	case "logfmt", "":
+	case "logfmt":
 		return zaplogfmt.NewEncoder(config), nil
 	default:
 		return nil, fmt.Errorf("unknown logging format: %s", format)
@@ -53,4 +62,13 @@ func newEncoderConfig() zapcore.EncoderConfig {
 		encoder.AppendString(d.String())
 	}
 	return config
+}
+
+func isTerminal(w io.Writer) bool {
+	if f, ok := w.(interface {
+		Fd() uintptr
+	}); ok {
+		return isatty.IsTerminal(f.Fd())
+	}
+	return false
 }
